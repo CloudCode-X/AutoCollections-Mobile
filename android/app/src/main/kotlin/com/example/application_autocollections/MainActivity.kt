4cd781fbf9e1735a0b660ad99dd5cdfe10bb232d
Rafael Rocha <rafinhaorocha21@gmail.com>
2025-11-30 00:23:43 -0300

    atualizando versão do gradle do android e atualizando dados json.

--- android/app/src/main/kotlin/com/example/application_autocollections/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.application_autocollections
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/autocollections_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.autocollections_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
